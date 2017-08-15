cd131aa03427fb7241708eddd809eb8021a7be4b
jesonn <448420599@qQ.com>
2017-08-15 09:11:52 +0800

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,9 @@
+// rollup.config.js
+import resolve from 'rollup-plugin-node-resolve';
+
+export default {
+    entry: 'src/main.js',
+    format: 'cjs',
+    plugins: [ resolve() ],
+    dest: 'bundle.js'
+};
